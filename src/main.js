a752a5ec0f2d56fa98dd511d504588e3f3c79e50
John Hungerford <jhungerford@gmail.com>
2021-05-20 05:52:23 -0600

    Use Quasar UI framework.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { invoke } from '@tauri-apps/api/tauri'
 import App from './App.vue'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
 invoke('sample_command')
-createApp(App).mount('#app')
+createApp(App).use(Quasar, quasarUserOptions).mount('#app')
