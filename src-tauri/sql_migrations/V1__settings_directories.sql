a7e69adf1036d01767cf396ae7f2e3e5b1ceed56
John Hungerford <jhungerford@gmail.com>
2021-11-06 07:14:33 -0600

    Fresh start.

--- src-tauri/sql_migrations/V1__settings_directories.sql
+++
@@ -1,4 +0,0 @@
--- This migration creates a table to store the directories that stl-organizer will scan for 3D printing files.
-CREATE TABLE directories (
-    name VARCHAR
-);
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { Quasar } from 'quasar'
-import { invoke } from '@tauri-apps/api/tauri'
-import quasarUserOptions from './quasar-user-options'
-
-
-invoke('add_dir', {'dir': '~/sample-js'})
-createApp(App).use(Quasar, quasarUserOptions).mount('#app')
