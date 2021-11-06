75615142f642d3f466e3e58ea9ed8c8502a93507
John Hungerford <jhungerford@gmail.com>
2021-11-06 08:50:21 -0600

    Hello world for webview.

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ func main() {
 	fmt.Println("Hello, World")
 
 	debug := true
+
 	w := webview.New(debug)
 	defer w.Destroy()
+
 	w.SetTitle("Minimal Webview Example")
 	w.SetSize(800, 600, webview.HintNone)
+
 	w.Navigate("https://en.m.wikipedia.org/wiki/Main_Page")
+
 	w.Run()
 }
